4764290a4cbd8ffc3c565c93b930250717713643
Skarlett <3759687+Skarlett@users.noreply.github.com>
2025-07-28 14:00:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="deemix-stream",
     version="0.1.0",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "deemix",
