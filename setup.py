ab620609b226f21396ffcd40bd1c4a90bda7e1a9
Skarlett <Skarlett@users.noreply.github.com>
2025-07-28 08:54:09 -0500

    init

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="deemix-stream",
+    version="0.1.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "deemix",
+        "requests",
+        "spotipy",
+        "click",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "deemix-stream=deemix_stream.__main__:stream",
+            "deemix-metadata=deemix_stream.__main__:metadata",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
